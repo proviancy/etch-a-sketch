51aa4a17bcfde989ba20c6754e158e442e7b84b9
cooper <mccoy.cooperf@gmail.com>
2023-01-07 11:06:00 -0500

    Add ability to change grid size

--- etchasketch.js
+++ etchasketch.js
@@ -1,10 +1,13 @@
 const grid = document.querySelector('.sketch');
 const resetButton = document.querySelector('.reset');
+const sizeButton = document.querySelector('.changeSize');
 
 const rowDiv = [];
 const colDiv = [];
 
 function createGrid(dimension = 16) {
+    
+    deleteGrid();
     
     for (let i = 0; i < dimension; i++) {
 
@@ -26,4 +29,17 @@ function createGrid(dimension = 16) {
 
 }
 
+function deleteGrid() {
+    if (rowDiv.length > 0) {
+        for (let i = rowDiv.length - 1; i >= 0; i--) {
+            // rowDiv[i].remove();
+            grid.firstElementChild.remove();
+        }
+    }
+}
+
 resetButton.addEventListener('click', () => createGrid());
+sizeButton.addEventListener('click', function (e) {
+    let size = prompt("Please enter a positive number between 1 and 100:");
+    createGrid(size);
+});
